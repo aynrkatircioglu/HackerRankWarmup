7a59718c8f5afc94e2adf8de19db6c398a323d9b
Aynur Katırcıoğlu <aynrkatircioglu@gmail.com>
2022-02-19 22:33:54 +0300

    AVeryBigSum added.

---
+++ AVeryBigSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVeryBigSum
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            var finalSum = 0L;
+            var x = int.Parse(Console.ReadLine());
+            var arrayTemp = Console.ReadLine().Split(' ');
+            var array = Array.ConvertAll(arrayTemp, long.Parse);
+
+            for (int i = 0; i < x; i++)
+                finalSum += array[i];
+
+            Console.WriteLine(finalSum);
+        }
+    }
+}
