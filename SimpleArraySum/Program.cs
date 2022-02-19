b76067ee862c1fc6b26026295366440bcc721801
Aynur Katırcıoğlu <aynrkatircioglu@gmail.com>
2022-02-19 22:22:08 +0300

    Add project files.

---
+++ SimpleArraySum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleArraySum
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.ReadLine();
+            var ar_temp = Console.ReadLine().Split(' ');
+            var ar = Array.ConvertAll(ar_temp, Int32.Parse);
+            Console.WriteLine(ar.Sum()); 
+        }
+    }
+}
---
+++ SolveMeFirst/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveMeFirst
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            var num1 = int.Parse(Console.ReadLine());
+            var num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
+
