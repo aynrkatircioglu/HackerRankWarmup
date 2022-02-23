8cae4f7d43717778e342437c3ed64f996cf30582
Aynur Katırcıoğlu <aynrkatircioglu@gmail.com>
2022-02-23 12:26:27 +0300

    TimeConversion added

---
+++ BirthdayCakeCandles/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayCakeCandles
+{
+    class Program
+    {
+        /* https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen */
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            var tempHeight = Console.ReadLine().Split(' ');
+            var height = Array.ConvertAll(tempHeight, int.Parse);
+            var maxValue = height[0];
+            var maxValueOccurence = 1;
+
+            for (int i = 1; i < height.Length; i++)
+            {
+                if (height[i] == maxValue)
+                {
+                    maxValueOccurence++;
+                    continue;
+                }
+                if (height[i] > maxValue)
+                {
+                    maxValue = height[i];
+                    maxValueOccurence = 1;
+                }
+            }
+            Console.WriteLine(maxValueOccurence);
+        }
+    }
+}
---
+++ TimeConversion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        /* https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen */
+        static void Main(string[] args)
+        {
+            var time = Console.ReadLine();
+            var pmOrAm = time.Substring(8);
+            var hourComponent = time.Substring(0, 2);
+            var remainingTime = time.Substring(2, 6);
+
+            if (pmOrAm == "AM" && hourComponent == "12")
+            {
+                hourComponent = "00";
+            }
+            else if (pmOrAm == "PM")
+            {
+                var numericHour = int.Parse(hourComponent);
+                if (numericHour != 12)
+                {
+                    hourComponent = Convert.ToString(12 + numericHour);
+                }
+            }
+            Console.WriteLine(hourComponent + remainingTime);
+        }
+    }
+}
